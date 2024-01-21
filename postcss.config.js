84fa0b83f11fdc2624e51a883106f253b986f824
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-01-21 00:34:38 +0000

    added features to pstcss config

--- postcss.config.js
+++ postcss.config.js
@@ -4,4 +4,9 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
   },
+  /* use stage 2 features + disable logical properties and values rule */
+  stage: 2,
+  features: {
+    "logical-properties-and-values": false,
+  },
 };
