febb81f8821e3623a45adbcfb86015a6a846a212
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-02-28 23:05:12 +0000

    fixed mounting issue for sessionStorage

--- components/useServiceOrder.ts
+++ components/useServiceOrder.ts
@@ -2,17 +2,22 @@
 
 import { useCallback, useEffect, useState } from "react";
 import type { serviceType } from "@/types";
-
 const useServiceOrder = (initialServices: serviceType[]) => {
+  // Initialize services state with a function to avoid accessing sessionStorage during SSR
   const [services, setServices] = useState<serviceType[]>(() => {
-    // Attempt to get a stored order from sessionStorage
-    const storedServices = sessionStorage.getItem("servicesOrder");
-    return storedServices ? JSON.parse(storedServices) : initialServices;
+    if (typeof window !== "undefined") {
+      // Attempt to get a stored order from sessionStorage, but only if window is defined
+      const storedServices = sessionStorage.getItem("servicesOrder");
+      return storedServices ? JSON.parse(storedServices) : initialServices;
+    }
+    return initialServices; // Fallback to initialServices if window is not available
   });
 
-  // Update sessionStorage whenever services order changes
+  // Update sessionStorage whenever services order changes, also checking for window availability
   useEffect(() => {
-    sessionStorage.setItem("servicesOrder", JSON.stringify(services));
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem("servicesOrder", JSON.stringify(services));
+    }
   }, [services]);
 
   const reorderService = useCallback((clickedIndex: number) => {
