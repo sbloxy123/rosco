321d05f8e1ba3660320d05efba2d765ade678de1
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-02-29 01:41:41 +0000

    config update

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "sanity";
-import { deskTool } from "sanity/desk";
+import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./schemas";
 import { orderableDocumentListDeskItem } from "@sanity/orderable-document-list";
@@ -12,7 +12,7 @@ export default defineConfig({
   dataset: "production",
   basePath: "/studio",
   CORS: ["https://rosco-liard.vercel.app"],
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
