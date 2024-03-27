9f638dc23c57eca3fdd21d0b6f26b56e0d72a8c0
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-03-27 18:00:29 +0000

    another border fix

--- components/utils/BorderedImageAbout.tsx
+++ components/utils/BorderedImageAbout.tsx
@@ -11,9 +11,67 @@ export default function BorderedImageAbout({
 }) {
   return (
     <div className="relative w-full h-auto small:max-w-[700px] medium:mr-0 medium:ml-auto">
-      <div className="relative w-[100vw] aspect-[349/375] -ml-[4.7%] -mt-[5.4%] xsmall:aspect-[690/451] xsmall:w-[104.5vw] xsmall:ml-[-6.9%] xsmall:-mt-[4.5%] small:ml-[-4.6%] small:absolute small:top-0 small:left-0 small:w-[109.4%] small:h-auto small:mt-[-4.1%] small:aspect-[703/454]">
+      <div className="relative w-[100vw] aspect-[349/375] -ml-[4.7%] -mt-[5.8%] xsmall:aspect-[690/451] xsmall:w-[104.5vw] xsmall:ml-[-6.9%] xsmall:-mt-[4.5%] small:ml-[-4.6%] small:absolute small:top-0 small:left-0 small:w-[109.4%] small:h-auto small:mt-[-4.1%] small:aspect-[703/454]">
         {/* 2 x mobile only border svgs */}
         <svg
+          viewBox="0 0 386 412"
+          fill="none"
+          className="mix-blend-color-dodge absolute top-0 w-full h-full xsmall:hidden"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16.5 20H365.5V392H16.5V20Z"
+            stroke="url(#aboutBorderMob)"
+            strokeOpacity="0.5"
+            strokeWidth="40"
+          />
+          <defs>
+            <linearGradient
+              id="aboutBorderMob"
+              x1="383.689"
+              y1="-76.8917"
+              x2="-41.2097"
+              y2="128.621"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0.161927" stopColor="#4804F8" />
+              <stop offset="0.375" stopColor="#FF4A1D" />
+              <stop offset="0.635417" stopColor="#F9BA17" />
+              <stop offset="0.978225" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <svg
+          viewBox="0 0 386 412"
+          fill="none"
+          className="mix-blend-color-dodge absolute top-0 w-full h-full -z-10 xsmall:hidden"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16.5 20H365.5V392H16.5V20Z"
+            stroke="url(#aboutBorderMob2)"
+            strokeOpacity="0.5"
+            strokeWidth="40"
+          />
+          <defs>
+            <linearGradient
+              id="aboutBorderMob2"
+              x1="383.689"
+              y1="-76.8917"
+              x2="-41.2097"
+              y2="128.621"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop offset="0.161927" stopColor="#4804F8" />
+              <stop offset="0.375" stopColor="#FF4A1D" />
+              <stop offset="0.635417" stopColor="#F9BA17" />
+              <stop offset="0.978225" stopOpacity="0" />
+            </linearGradient>
+          </defs>
+        </svg>
+
+        {/* --------------------------- */}
+        {/* <svg
           viewBox="0 0 385 412"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -76,7 +134,7 @@ export default function BorderedImageAbout({
               <stop offset="0.978225" stopOpacity="0" />
             </linearGradient>
           </defs>
-        </svg>
+        </svg> */}
 
         {/* 2 x TABLET+ SCREEN border svgs */}
 
@@ -204,7 +262,7 @@ export default function BorderedImageAbout({
           </defs>
         </svg>
       </div>
-      <div className="absolute top-[-0.4%] left-[0.6%] w-[98.7%] aspect-[343/365] overflow-hidden xsmall:w-[calc(100%+0.6%)] xsmall:left-[-0.5%]  xsmall:absolute xsmall:aspect-[690/451] small:w-[calc(100%+1.2%)] small:relative small:aspect-[703/454] small:mt-0">
+      <div className="absolute top-[0.1%] left-[0%] w-[98.5%] aspect-[342.7/365] overflow-hidden xsmall:w-[calc(100%+0.6%)] xsmall:left-[-0.5%]  xsmall:absolute xsmall:aspect-[690/451] small:w-[calc(100%+1.2%)] small:relative small:aspect-[703/454] small:mt-0">
         <Image
           src={getCroppedImageSrc(image)}
           alt={image.alt}
