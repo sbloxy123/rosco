4c6597ae7e84936cbb29839d768a12a55b4fbf42
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-05-23 21:41:30 +0100

    fixed main title size and aligned inner titles to left on policy ant terms pages

--- app/(site)/privacy-policy/page.tsx
+++ app/(site)/privacy-policy/page.tsx
@@ -9,9 +9,9 @@ export default async function privacyPolicy() {
   return (
     <main className="mt-[10rem] max-w-[1120px] mx-auto px-[5%] small:px-layout-small">
       <section className="max-w-[1120px] mx-auto pt-[4rem] small:pt-[7rem]">
-        <h1 className="text-center xsmall:max-w-[80%] mx-auto">
+        <h2 className="text-center xsmall:max-w-[80%] mx-auto text-[3rem]">
           {data[0].policies.pageTitle}
-        </h1>
+        </h2>
         <div className="rich-text-container pt-[4rem]">
           <PortableText value={data[0].policies.policies} />
         </div>
