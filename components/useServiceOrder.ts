50737272e16acc8f7038c250865a799c2c17a350
Stuart Bloxham <s.bloxham@hotmail.co.uk>
2024-05-28 22:59:02 +0100

    fixed hydration error in services by updating service list ordering code

--- components/useServiceOrder.ts
+++ components/useServiceOrder.ts
@@ -1,30 +1,35 @@
 "use client";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { serviceType } from "@/types";
 
 const useServiceOrder = (initialServices: serviceType[]) => {
-  const [services, setServices] = useState<serviceType[]>(() => {
-    if (typeof window !== "undefined") {
-      const storedServices = sessionStorage.getItem("servicesOrder");
-      return storedServices ? JSON.parse(storedServices) : initialServices;
-    }
-    return initialServices;
-  });
+  const [services, setServices] = useState<serviceType[]>(initialServices);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
+    setIsMounted(true);
     if (typeof window !== "undefined") {
+      const storedServices = sessionStorage.getItem("servicesOrder");
+      if (storedServices) {
+        setServices(JSON.parse(storedServices));
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isMounted && typeof window !== "undefined") {
       sessionStorage.setItem("servicesOrder", JSON.stringify(services));
     }
-  }, [services]);
+  }, [services, isMounted]);
 
-  const reorderService = useCallback((clickedIndex: number) => {
+  const reorderService = (clickedIndex: number) => {
     setServices((currentServices) => {
       const updatedServices = [...currentServices];
       const itemToMove = updatedServices.splice(clickedIndex, 1)[0];
       updatedServices.push(itemToMove);
       return updatedServices;
     });
-  }, []);
+  };
 
   return { services, reorderService };
 };
